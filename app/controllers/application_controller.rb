7173601ef46f57d8e00c3ee7a47a92bedbd44184
theory friction practice <jackson.brebnor@gmail.com>
2015-03-01 18:53:02 -0500

    sets up the header and major applicationController variables forsharedPartialzzz yassssssss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :setMenuLinks
+
+  def setMenuLinks
+  	@sharedSections = Section.includes(:serials, :articles).all
+  	
+  end
+  
+
 end
